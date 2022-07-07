618dd8abeb5781c1e4d7c5b2d601c5ca3d68878e
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-06 20:55:27 -0500

    The @DataJpaTest annotation on the RepositoryTest class does start the test and connects us to the h2 in-memory database

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -2,10 +2,12 @@ package com.example.demo.student;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
