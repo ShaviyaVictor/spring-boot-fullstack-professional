22c1781066733e6203319e79a23aa4d5243065cb
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-07 11:32:03 -0500

    Create the canAddStudentTest method using the given, when and then analogy and introducing the ArgumentCaptor dependency

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -4,31 +4,35 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
 
 // import not in use since we are using AssertJ for methods
 // import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock
     private StudentRepository studentRepository;
-    private AutoCloseable autoCloseable;
+    // private AutoCloseable autoCloseable;
     private StudentService underTest;
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
+        // autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new StudentService(studentRepository);
     }
 
-    @AfterEach
+    /*@AfterEach
     void tearDown() throws Exception {
         autoCloseable.close();
-    }
+    }*/
 
     @Test
     void canGetAllStudents() {
@@ -39,8 +43,24 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void canAddStudent() {
+        // given
+        String email = "shavic@gmail.com";
+        Student student = new Student(
+                "Shaviya",
+                email,
+                Gender.MALE
+        );
+        // when
+        underTest.addStudent(student);
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        
+
     }
 
     @Test
