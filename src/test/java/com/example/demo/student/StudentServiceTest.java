e9384001797acd877b5f02e3981540599357a6cf
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-07 08:16:46 -0500

    Add @Disabled annotation to disable the tests not being run at the moment

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +12,12 @@ class StudentServiceTest {
     }
 
     @Test
+    @Disabled
     void addStudent() {
     }
 
     @Test
+    @Disabled
     void deleteStudent() {
     }
 }
