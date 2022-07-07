2de8e4d25fda7c867c870941d61a26842924d343
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-06 21:04:15 -0500

    Create a tearDown method to clear the data after each test is run

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +13,11 @@ class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
 
     @Test
     void itShouldCheckIfStudentEmailExists() {
