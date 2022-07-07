38259a593808c5f109dcd99451124e4afe36f07b
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-07 08:23:47 -0500

    Create a setUp method with @BeforeEach annotation for the getAllStudentsTestCase

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,11 +1,21 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import not in use since we are using AssertJ for methods
+// import static org.junit.jupiter.api.Assertions.*;
 
 class StudentServiceTest {
+
+    private StudentRepository studentRepository;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new StudentService(studentRepository);
+    }
 
     @Test
     void getAllStudents() {
